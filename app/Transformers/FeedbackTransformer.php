2420fd961391e242826e4bdc07e82cdda6eacd5b
Joseph Akitoye <josephakitoye@gmail.com>
2021-01-29 13:30:04 +0000

    Add device fields to transformer
    Signed-off-by: Joseph Akitoye <josephakitoye@gmail.com>

--- app/Transformers/FeedbackTransformer.php
+++ app/Transformers/FeedbackTransformer.php
@@ -22,46 +22,50 @@ class FeedbackTransformer extends TransformerAbstract
      * @var array
      */
     protected $availableIncludes = [
-        'company','customer'
+        'company', 'customer'
     ];
 
     /**
      * A Fractal transformer.
      *
      * @param Feedback $feedback
+     *
      * @return array
      */
-    public function transform( Feedback $feedback ): array
+    public function transform(Feedback $feedback): array
     {
         return [
-            'id' => $feedback->getAttribute( 'id' ),
-            'message' => $feedback->getAttribute( 'message' ),
-            'photo' => $this->getImageUrl( $feedback ),
-            'company_id' => $feedback->company_id,
-            'customer_name' => $feedback->customer->name,
-            'phone_number' => $feedback->customer->phone_number,
+            'id'               => $feedback->getAttribute('id'),
+            'message'          => $feedback->getAttribute('message'),
+            'photo'            => $this->getImageUrl($feedback),
+            'company_id'       => $feedback->company_id,
+            'customer_name'    => $feedback->customer->name,
+            'phone_number'     => $feedback->customer->phone_number,
             'customer_address' => $feedback->customer->address,
-            'snoocode' => $feedback->customer->snoocode,
+            'snoocode'         => $feedback->customer->snoocode,
+            'app_version'      => $feedback->getAttribute('app_version'),
+            'unique_id'        => $feedback->getAttribute('unique_id'),
+            'device_id'        => $feedback->getAttribute('device_id'),
         ];
     }
 
-    private function getImageUrl( Feedback $feedback )
+    private function getImageUrl(Feedback $feedback): ?string
     {
-        if ( $feedback->getAttribute( 'photo' ) == null ) {
+        if ( $feedback->getAttribute('photo') == null ) {
             return null;
         }
 
-        return Storage::disk( 's3' )->url( 'feedback/' . $feedback->getAttribute( 'photo' ) );
+        return Storage::disk('s3')->url('feedback/' . $feedback->getAttribute('photo'));
     }
 
-    public function includeCustomer( Feedback $feedback ): Item
+    public function includeCustomer(Feedback $feedback): Item
     {
-        return $this->item( $feedback->customer, new CustomerTransformer, 'customers' );
+        return $this->item($feedback->customer, new CustomerTransformer, 'customers');
     }
 
-    public function includeCompany( Feedback $feedback )
+    public function includeCompany(Feedback $feedback): ?Item
     {
-        if ( is_null( $feedback->company ) ) return null;
-        return $this->item( $feedback->company, new CompanyTransformer, 'companies' );
+        if ( is_null($feedback->company) ) return null;
+        return $this->item($feedback->company, new CompanyTransformer, 'companies');
     }
 }
