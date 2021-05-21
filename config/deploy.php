abbd55d6ecc5c57f7c0fde8d3bd9dc1549193877
Joseph Akitoye <j.akitoye@tinydavid.com>
2021-05-21 10:03:27 +0000

    Removed Horizon and edited SSH keys

--- config/deploy.php
+++ config/deploy.php
@@ -93,7 +93,7 @@ return [
 
     'options' => [
         'application' => env('APP_NAME', 'Laravel'),
-        'repository' => 'https://github.com/CleanKenya/api',
+        'repository' => 'git@github.com:CleanKenya/api.git',
         'php_fpm_service' => 'php7.4-fpm',
     ],
 
