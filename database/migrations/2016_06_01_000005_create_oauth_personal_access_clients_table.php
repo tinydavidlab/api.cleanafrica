3b2e8ad16c89026470d5bc9c0ebdfdd8bc7d804f
Joseph Akitoye <josephakitoye@gmail.com>
2022-11-16 07:47:55 +0000

    Disable primary key required for migrations for passport
    Signed-off-by: Joseph Akitoye <josephakitoye@gmail.com>

---
+++ database/migrations/2016_06_01_000005_create_oauth_personal_access_clients_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * The database schema.
+     *
+     * @var \Illuminate\Database\Schema\Builder
+     */
+    protected $schema;
+
+    /**
+     * Create a new migration instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->schema = Schema::connection( $this->getConnection() );
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement( 'SET SESSION sql_require_primary_key=0' );
+        $this->schema->create( 'oauth_personal_access_clients', function ( Blueprint $table ) {
+            $table->bigIncrements( 'id' );
+            $table->unsignedBigInteger( 'client_id' );
+            $table->timestamps();
+        } );
+        DB::statement( 'SET SESSION sql_require_primary_key=1' );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->dropIfExists( 'oauth_personal_access_clients' );
+    }
+
+    /**
+     * Get the migration connection name.
+     *
+     * @return string|null
+     */
+    public function getConnection()
+    {
+        return config( 'passport.storage.database.connection' );
+    }
+};
