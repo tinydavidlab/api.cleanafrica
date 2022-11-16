06c933dd5d686725a0ea54f66f3534a1cca9d837
Joseph Akitoye <josephakitoye@gmail.com>
2022-11-16 07:40:31 +0000

    Add lumen routes to service provider
    Signed-off-by: Joseph Akitoye <josephakitoye@gmail.com>

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,8 +30,8 @@ class AuthServiceProvider extends ServiceProvider
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
-//        LumenPassport::routes( $this->app );
-//        LumenPassport::allowMultipleTokens();
+        LumenPassport::routes( $this->app );
+        LumenPassport::allowMultipleTokens();
         LumenPassport::tokensExpireIn( Carbon::now()->addYears( 1 ) );
     }
 }
