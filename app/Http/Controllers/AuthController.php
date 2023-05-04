ec2de36496730a3f2760dc16d07b32be3f38c604
Emmanuel Asiamah Owusu <vfgh@Emmanuels-MacBook-Pro-2.local>
2023-05-04 00:33:57 +0000

    Update device token field every time user logs in

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -49,11 +49,11 @@ class AuthController extends Controller
      * @param AdminRepository $adminRepository
      * @param AgentRepository $agentRepository
      */
-    public function __construct( CustomerRepository $customerRepository, AdminRepository $adminRepository, AgentRepository $agentRepository )
+    public function __construct(CustomerRepository $customerRepository, AdminRepository $adminRepository, AgentRepository $agentRepository)
     {
         $this->customerRepository = $customerRepository;
-        $this->adminRepository    = $adminRepository;
-        $this->agentRepository    = $agentRepository;
+        $this->adminRepository = $adminRepository;
+        $this->agentRepository = $agentRepository;
     }
 
     /**
@@ -64,61 +64,61 @@ class AuthController extends Controller
      * @throws ValidationException
      * @throws Exception
      */
-    public function login( Request $request ): JsonResponse
+    public function login(Request $request): JsonResponse
     {
-        $this->validate( $request, [
-            'username'      => 'required',
-            'grant_type'    => 'in:password',
-            'client_id'     => 'required',
+        $this->validate($request, [
+            'username' => 'required',
+            'grant_type' => 'in:password',
+            'client_id' => 'required',
             'client_secret' => 'required',
-            'type'          => 'in:customer,collector,admin,super_admin',
-        ] );
+            'type' => 'in:customer,collector,admin,super_admin',
+        ]);
 
-        $type = $request->get( 'type', 'admin' );
+        $type = $request->get('type', 'admin');
 
         $credentials = [
-            'grant_type'    => $request->get( 'grant_type', 'password' ),
-            'username'      => $request->get( 'username' ),
-            'password'      => $request->get( 'username' ),
-            'client_id'     => $request->get( 'client_id' ),
-            'client_secret' => $request->get( 'client_secret' ),
-            'scope'         => $request->get( 'scope', '' ),
+            'grant_type' => $request->get('grant_type', 'password'),
+            'username' => $request->get('username'),
+            'password' => $request->get('username'),
+            'client_id' => $request->get('client_id'),
+            'client_secret' => $request->get('client_secret'),
+            'scope' => $request->get('scope', ''),
         ];
 
-        return $this->handleAuthentication( $credentials, $request, $type );
+        return $this->handleAuthentication($credentials, $request, $type);
     }
 
-    protected function getUserForGrantType( string $type, array $auth_token )
+    protected function getUserForGrantType(string $type, array $auth_token)
     {
-        $access_token = Arr::get( $auth_token, 'access_token' );
+        $access_token = Arr::get($auth_token, 'access_token');
 
-        if ( !$jwt = $access_token ) return null;
-        [ $headb64, $bodyb64, $cryptob64 ] = explode( '.', $jwt );
-        $body = JWT::jsonDecode( JWT::urlsafeB64Decode( $bodyb64 ) );
-        if ( !$id = data_get( $body, 'sub' ) ) return null;
+        if (!$jwt = $access_token) return null;
+        [$headb64, $bodyb64, $cryptob64] = explode('.', $jwt);
+        $body = JWT::jsonDecode(JWT::urlsafeB64Decode($bodyb64));
+        if (!$id = data_get($body, 'sub')) return null;
 
-        if ( $type == 'customer' ) {
-            return Customer::find( $id );
-        } elseif ( $type == 'collector' ) {
-            return Agent::find( $id );
+        if ($type == 'customer') {
+            return Customer::find($id);
+        } elseif ($type == 'collector') {
+            return Agent::find($id);
         }
 
-        return Admin::find( $id );
+        return Admin::find($id);
     }
 
-    private function getTransformedUserType( $user, $type = 'admin' ): array
+    private function getTransformedUserType($user, $type = 'admin'): array
     {
-        if ( $type == 'collector' ) {
-            $user = fractal( $user, new AgentTransformer() )
-                ->withResourceName( Str::plural( $type ) )
+        if ($type == 'collector') {
+            $user = fractal($user, new AgentTransformer())
+                ->withResourceName(Str::plural($type))
                 ->toArray();
-        } else if ( $type == 'customer' ) {
-            $user = fractal( $user, new CustomerTransformer() )
-                ->withResourceName( Str::plural( $type ) )
+        } else if ($type == 'customer') {
+            $user = fractal($user, new CustomerTransformer())
+                ->withResourceName(Str::plural($type))
                 ->toArray();
         } else {
-            $user = fractal( $user, new AdminTransformer() )
-                ->withResourceName( Str::plural( $type ) )
+            $user = fractal($user, new AdminTransformer())
+                ->withResourceName(Str::plural($type))
                 ->toArray();
         }
 
@@ -133,29 +133,29 @@ class AuthController extends Controller
      * @param Request $request
      * @return JsonResponse
      */
-    protected function respondWithToken( string $type, string $token, Request $request ): JsonResponse
+    protected function respondWithToken(string $type, string $token, Request $request): JsonResponse
     {
-        $payload = JWTAuth::setToken( $token )->getPayload();
+        $payload = JWTAuth::setToken($token)->getPayload();
 
         $token = [
             'access_token' => $token,
-            'token_type'   => 'bearer',
-            'expires_in'   => $payload->get( 'exp' ),
+            'token_type' => 'bearer',
+            'expires_in' => $payload->get('exp'),
         ];
 
-        $user = auth()->guard( $type )->user();
-        if ( $request->has( 'device_token' ) ) {
-            $user->update( [ 'device_token' => $request->get( 'device_token' ) ] );
+        $user = auth()->guard($type)->user();
+        if ($request->has('device_token')) {
+            $user->update(['device_token' => $request->get('device_token')]);
         }
 
-        $user = $this->getTransformedUserType( $user, $type );
+        $user = $this->getTransformedUserType($user, $type);
 
-        return response()->json( [
+        return response()->json([
             'auth' => [
                 'token' => $token,
-                $type   => $user
+                $type => $user
             ]
-        ] );
+        ]);
     }
 
     /**
@@ -165,9 +165,9 @@ class AuthController extends Controller
      */
     public function me(): JsonResponse
     {
-        $guard = request( 'type' );
-        $me    = fractal( auth()->guard( $guard )->user(), $guard == 'customer' ? new CustomerTransformer : new AdminTransformer )->withResourceName( Str::plural( $guard ) )->toArray();
-        return response()->json( [ 'me' => $me ] );
+        $guard = request('type');
+        $me = fractal(auth()->guard($guard)->user(), $guard == 'customer' ? new CustomerTransformer : new AdminTransformer)->withResourceName(Str::plural($guard))->toArray();
+        return response()->json(['me' => $me]);
     }
 
     /**
@@ -179,7 +179,7 @@ class AuthController extends Controller
     {
         auth()->logout();
 
-        return response()->json( [ 'message' => 'Successfully logged out' ] );
+        return response()->json(['message' => 'Successfully logged out']);
     }
 
     /**
@@ -189,7 +189,7 @@ class AuthController extends Controller
      */
     public function refresh(): JsonResponse
     {
-        return $this->respondWithToken( auth()->refresh() );
+        return $this->respondWithToken(auth()->refresh());
     }
 
     /**
@@ -198,41 +198,41 @@ class AuthController extends Controller
      * @throws ValidationException
      * @throws ValidatorException
      */
-    public function register( Request $request ): JsonResponse
+    public function register(Request $request): JsonResponse
     {
-        $table = Str::plural( $request->get( 'type', 'customer' ) );
-        $table = ( $table == "collectors" ) ? "admins" : ( ( $table == "super_admins" ) ? "admins" : $table );
+        $table = Str::plural($request->get('type', 'customer'));
+        $table = ($table == "collectors") ? "admins" : (($table == "super_admins") ? "admins" : $table);
 
-        $this->validate( $request, [
-            'name'          => 'required',
-            'type'          => 'required|in:customer,collector,admin,super_admin',
-            'phone_number'  => 'required|unique:' . $table . ',phone_number',
-            'company_id'    => 'exists:companies,id',
-            'grant_type'    => 'in:password',
-            'client_id'     => 'required',
+        $this->validate($request, [
+            'name' => 'required',
+            'type' => 'required|in:customer,collector,admin,super_admin',
+            'phone_number' => 'required|unique:' . $table . ',phone_number',
+            'company_id' => 'exists:companies,id',
+            'grant_type' => 'in:password',
+            'client_id' => 'required',
             'client_secret' => 'required',
-            'scope'         => 'nullable',
+            'scope' => 'nullable',
         ], [], [
             'type' => 'validation',
             'code' => Response::HTTP_UNPROCESSABLE_ENTITY
-        ] );
+        ]);
 
-        $type = $request->get( 'type' );
+        $type = $request->get('type');
 
-        $user = $this->createUserWithType( $request->all(), $type );
+        $user = $this->createUserWithType($request->all(), $type);
 
         $credentials = [
-            'grant_type'    => $request->get( 'grant_type', 'password' ),
-            'username'      => $request->get( 'phone_number' ),
-            'password'      => $request->get( 'phone_number' ),
-            'client_id'     => $request->get( 'client_id', env('CLIENT_ID') ),
-            'client_secret' => $request->get( 'client_secret', env('CLIENT_SECRET') ),
-            'scope'         => $request->get( 'scope', '' ),
+            'grant_type' => $request->get('grant_type', 'password'),
+            'username' => $request->get('phone_number'),
+            'password' => $request->get('phone_number'),
+            'client_id' => $request->get('client_id', env('CLIENT_ID')),
+            'client_secret' => $request->get('client_secret', env('CLIENT_SECRET')),
+            'scope' => $request->get('scope', ''),
         ];
 
-        event( new NewUserRegistered( $user ) );
+        event(new NewUserRegistered($user));
 
-        return $this->handleAuthentication( $credentials, $request, $type );
+        return $this->handleAuthentication($credentials, $request, $type);
     }
 
     /**
@@ -241,26 +241,26 @@ class AuthController extends Controller
      * @return LengthAwarePaginator|Collection|mixed
      * @throws ValidatorException
      */
-    private function createUserWithType( array $data, string $type )
+    private function createUserWithType(array $data, string $type)
     {
 
-        if ( $type == 'customer' ) {
+        if ($type == 'customer') {
             return $this->customerRepository->create(
                 array_merge(
                     $data,
-                    [ 'password' => Hash::make( $data[ 'phone_number' ] ),
-                        'snoocode' => Arr::get( $data, 'code' ) ]
+                    ['password' => Hash::make($data['phone_number']),
+                        'snoocode' => Arr::get($data, 'code')]
                 )
             );
         }
 
-        if ( $type == 'collector' ) {
+        if ($type == 'collector') {
             return $this->agentRepository->create(
                 array_merge(
                     $data,
-                    [ 'password' => Hash::make( $data[ 'phone_number' ] ),
-                        'device_token' => $data[ 'device_token' ]
-                        ]
+                    ['password' => Hash::make($data['phone_number']),
+                        'device_token' => $data['device_token']
+                    ]
                 )
             );
         }
@@ -268,7 +268,7 @@ class AuthController extends Controller
         return $this->adminRepository->create(
             array_merge(
                 $data,
-                [ 'password' => Hash::make( $data[ 'phone_number' ] ) ]
+                ['password' => Hash::make($data['phone_number'])]
             )
         );
     }
@@ -279,28 +279,26 @@ class AuthController extends Controller
      * @param $type
      * @return JsonResponse
      */
-    protected function handleAuthentication( array $credentials, Request $requestFromApp, $type ): JsonResponse
+    protected function handleAuthentication(array $credentials, Request $requestFromApp, $type): JsonResponse
     {
-        $request    = Request::create( '/oauth/token', 'POST', $credentials );
-        $response   = app()->handle( $request );
-        $auth_token = json_decode( $response->getContent(), true );
-        if ( $response->getStatusCode() != Response::HTTP_OK ) {
-            return response()->json( [
-                'message' => Arr::get( $auth_token, 'message' )
-            ], Response::HTTP_BAD_REQUEST );
+        $request = Request::create('/oauth/token', 'POST', $credentials);
+        $response = app()->handle($request);
+        $auth_token = json_decode($response->getContent(), true);
+        if ($response->getStatusCode() != Response::HTTP_OK) {
+            return response()->json([
+                'message' => Arr::get($auth_token, 'message')
+            ], Response::HTTP_BAD_REQUEST);
         }
 
-        $user = $this->getUserForGrantType( $type, $auth_token );
+        $user = $this->getUserForGrantType($type, $auth_token);
 
-        if ($type == "collector") {
-            $this->agentRepository->update([ 'device_token' => $requestFromApp->get( 'device_token' ) ], $user->id );
-        }
+        $this->agentRepository->update(['device_token' => $requestFromApp->get('device_token')], $user->id);
 
-        return response()->json( [
+        return response()->json([
             'auth' => [
                 'token' => $auth_token,
-                $type   => $this->getTransformedUserType( $user, $type )
+                $type => $this->getTransformedUserType($user, $type)
             ]
-        ], Response::HTTP_OK );
+        ], Response::HTTP_OK);
     }
 }
